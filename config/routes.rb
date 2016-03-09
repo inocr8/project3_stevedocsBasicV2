3d0fa7bdacda7afc495ca7dc1a8cc6b8a41e4ed0
Euan Walls <apps@inocr8.co>
2016-03-09 20:39:26 +0000

    devise added with adjustment to normalize.css link, routes and dev environment, migration via Docker too

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  root 'home#index'
+  devise_for :users, :controllers => { registrations: 'registrations' }
+  root to: 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
