3a989c5d716d79a5ee9b913b69292abb10f80607
Euan Walls <apps@inocr8.co>
2016-03-09 22:29:48 +0000

    controller added for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,40 @@
 class UsersController < ApplicationController
-  def index
-  end
+  
+    before_action :load_user, except:[:index, :create, :new]
+    before_action :authenticate_user!
+    load_and_authorize_resource
 
-  def edit
-  end
+    def index
+      @users = User.all
+    end
 
-  def new
-  end
+    def show
+    end
 
-  def show
+    def edit
+    end
+
+    def update
+      respond_to do |format|
+        if @user.update(user_params)
+          format.html { redirect_to @user, notice: 'This user was successfully updated.' }
+        else
+          format.html { render :edit }
+        end
+      end
+    end
+
+    def destroy
+      @user.destroy
+      redirect_to(users_path)
+    end
+
+    private
+      def user_params
+        params.require(:user).permit(:name, :email, :role, :twitter, :company, :bio, :user_image)
+      end
+
+    def load_user
+      @user = User.find(params[:id])
+    end
   end
-end
